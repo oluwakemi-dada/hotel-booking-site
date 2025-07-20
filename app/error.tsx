426f753718e1d9c1a8602da745fbeff02af8cce3
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-07-20 19:41:34 +0100

    feat: error boundaries

---
+++ app/error.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+type Props = {
+  error: Error;
+  reset: () => void;
+};
+
+const Error = ({ error, reset }: Props) => {
+  return (
+    <main className='flex justify-center items-center flex-col gap-6'>
+      <h1 className='text-3xl font-semibold'>Something went wrong!</h1>
+      <p className='text-lg'>{error.message}</p>
+
+      <button
+        className='inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg cursor-pointer'
+        onClick={reset}
+      >
+        Try again
+      </button>
+    </main>
+  );
+};
+
+export default Error;
